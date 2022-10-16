ee95bff86ddcc7a794e7eac5cad36afd93171076
OttoBisno <ottobisno444@gmail.com>
2022-10-16 11:51:25 -0700

    Completed functions which provide license info, touched up remaining areas

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,5 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+// Function that returns a license badge based on which license is passed in
 function renderLicenseBadge(license) {
   switch(license) {
     case 'MIT':
@@ -17,15 +16,79 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Function that returns the license link
+function renderLicenseLink(license) {
+  switch(license) {
+    case 'MIT':
+      return '[License Link](https://opensource.org/licenses/MIT)';
+      break;
+    case 'Apache':
+      return '[License Link](https://opensource.org/licenses/Apache-2.0)';
+      break;
+    case 'BSD':
+      return '[License Link](https://opensource.org/licenses/BSD-2-Clause)';
+      break;
+    case 'No license for this application':
+      return '';
+      break;
+  }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+}
 
-// TODO: Create a function to generate markdown for README
+// Function that returns the license section of README
+function renderLicenseSection(license, username) {
+  switch(license) {
+    case 'MIT':
+      return `${renderLicenseLink(license)}<br>
+
+         Copyright 2022 ${username}
+
+      Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+      
+      The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+      
+      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`;
+      break;
+
+    case 'Apache':
+      return `${renderLicenseLink(license)}<br>
+
+         Copyright 2022 ${username}
+
+      Licensed under the Apache License, Version 2.0 (the "License");
+      you may not use this file except in compliance with the License.
+      You may obtain a copy of the License at
+   
+        http://www.apache.org/licenses/LICENSE-2.0
+   
+      Unless required by applicable law or agreed to in writing, software
+      distributed under the License is distributed on an "AS IS" BASIS,
+      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+      See the License for the specific language governing permissions and
+      limitations under the License.`;
+      break;
+
+    case 'BSD':
+      return `${renderLicenseLink(license)}<br>
+
+         Copyright 2022 ${username}
+
+      Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+      
+      1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+      
+      2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+      
+      THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.`;
+      break;
+
+    case 'No license for this application':
+      return 'N/A';
+      break;
+  }
+}
+
+// Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title} ${renderLicenseBadge(data.license)}
 
@@ -48,7 +111,7 @@ function generateMarkdown(data) {
   ${data.usage}
   
   ## License
-  ${data.license}
+  ${renderLicenseSection(data.license, data.github)}
   
   ## Contributing
   ${data.contributing}
@@ -57,7 +120,7 @@ function generateMarkdown(data) {
   ${data.tests}
   
   ## Questions
-  A link to my GitHub profile can be found here: [GitHub Profile](github.com/${data.github}<br>
+  A link to my GitHub profile can be found here: [GitHub Profile](github.com/${data.github})<br>
   For any questions regarding this application, please reach out to me directily at [${data.email}](mailto:${data.email})
 `;
 }
